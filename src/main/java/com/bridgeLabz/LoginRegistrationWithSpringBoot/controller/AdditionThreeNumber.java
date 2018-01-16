ddef00e40a63612f1e6e2de2f140af9b901687bc
prajapatideepak2210 <prajapatideepak2210>
2018-01-16 19:32:51 +0530

    commit

---
+++ src/main/java/com/bridgeLabz/LoginRegistrationWithSpringBoot/controller/AdditionThreeNumber.java
@@ -0,0 +1,5 @@
+package com.bridgeLabz.LoginRegistrationWithSpringBoot.controller;
+
+public class AdditionThreeNumber {
+
+}
--- src/test/java/com/bridgeLabz/LoginRegistrationWithSpringBoot/LoginRegistrationWithSpringBootApplicationTests.java
+++ src/test/java/com/bridgeLabz/LoginRegistrationWithSpringBoot/LoginRegistrationWithSpringBootApplicationTests.java
@@ -1,16 +1,34 @@
 package com.bridgeLabz.LoginRegistrationWithSpringBoot;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bridgeLabz.LoginRegistrationWithSpringBoot.controller.LoginController;
+import com.bridgeLabz.LoginRegistrationWithSpringBoot.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginRegistrationWithSpringBootApplicationTests {
 
+	@Autowired
+	LoginController loginController;
+	
+	
 	@Test
 	public void contextLoads() {
+		User user = new User();
+		
+			user.setUserName("deepak@gmail.com");
+			user.setPassword("deepak12");
+			
+			assertEquals(HttpStatus.ACCEPTED,loginController.login(user));
+		
 	}
 
 }
